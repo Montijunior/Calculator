acca51a57737514b67b416535fcf7a72f8a6dbb4
Monti junior <montiabouou@gmail.com>
2023-12-10 15:22:06 -0800

    initial index.js commit

---
+++ script/index.js
@@ -0,0 +1,16 @@
+//function add,subtract,multiply,divide
+const add = function(a,b){
+    return a + b;
+};
+
+const subtract = function(a,b){
+    return a - b;
+};
+
+const multiply = function(a,b){
+    return a * b;
+};
+
+const divide = function(a,b){
+    return a / b;
+};
