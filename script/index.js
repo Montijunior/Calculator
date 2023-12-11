f16113656afa4d8f72154e3799596486a3f28759
Monti junior <montiabouou@gmail.com>
2023-12-10 19:21:08 -0800

    refactoring code

--- script/index.js
+++ script/index.js
@@ -14,3 +14,28 @@ const multiply = function(a,b){
 const divide = function(a,b){
     return a / b;
 };
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(operator,a,b){
+    if (operator === '+') {
+        return add(a,b)
+    }
+    else if (operator === '-'){
+        return subtract(a,b);
+    } else if (operator === '*') {
+        return multiply(a,b);
+    } else if (operator === '/') {
+        return divide(a,b);
+    }
+    
+}
+
+
+const displayTop = document.querySelector('.top')
+const displayBottom = document.querySelector('.bottom');
+const clear = document.querySelector('.clear')
+const deleteBtn = document.querySelector('.delete');
+const equalsKey = document.querySelector('.answer');
